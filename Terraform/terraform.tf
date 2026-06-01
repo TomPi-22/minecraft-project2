dba131e47c84e3dd8457295d877067796ab930fe
TomPi-22 <tomc22mac@gmail.com>
2026-06-01 12:51:11 -0700

    Terraform providers and versions - terraform.tf

---
+++ Terraform/terraform.tf
@@ -0,0 +1,14 @@
+# Required providers and versions for the project
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}

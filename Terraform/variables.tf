d9e218cfc45be83342c6db24c7b6720325c1ed7b
TomPi-22 <tomc22mac@gmail.com>
2026-06-01 11:48:17 -0700

    Terraform file creation - variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,36 @@
+# Input variables for the AWS EC2 build
+# Allows for variable updates without manual reconfiguration
+
+variable "instance_name" {
+  description = "Value of the EC2 instance's Name tag."
+  type        = string
+  default     = "minecraft-server"
+}
+
+variable "region" {
+  description = "AWS region for instance"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "ami_id" {
+  description = "Ubuntu 24.04 (us-west-2)"
+  type        = string
+  default     = "ami-05cf1e9f73fbad2e2"
+}
+
+variable "instance_type" {
+  description = "The EC2 instance's type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name of the AWS key for SSH connections"
+  type        = string
+}
+
+
+
+
+
